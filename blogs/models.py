f413cf0aeafab14c4c1963e96b3d307d5566eea2
cKey <klvzcgkn@gmail.com>
2023-07-03 00:43:00 +0300

    urls path with include yapildi

--- blogs/models.py
+++ blogs/models.py
@@ -4,5 +4,14 @@ class BlogPost(models.Model):
     """."""
 
     title = models.CharField(max_length=200)
+    #title = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
+
+    """
+    def __str__(self):
+        #Return string BlogPost#docstring
+        if len(self.title) < 50:
+            return self.title
+        return f"{self.title[:50]}..."
+    """
