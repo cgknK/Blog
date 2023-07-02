0e0e512865762b5ff40ede336b4165d738ba2926
cKey <klvzcgkn@gmail.com>
2023-07-02 21:49:58 +0300

    BlogPost model create, migrate done, settings-uygulama eklendi-

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #My apps
+    'blogs',
+
+    # Default
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    """."""
+
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
