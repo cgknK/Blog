77ee9177f21e4e27a09dd3af417b5ea973532f1b
cKey <klvzcgkn@gmail.com>
2023-07-03 01:12:23 +0300

    All post list order by chronological on home page

---
+++ blogs/urls.py
@@ -0,0 +1,10 @@
+"""Descripe Urls pattern for blogs"""
+
+from django.urls import path
+from . import views
+
+app_name = 'blogs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+]
--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# .'tanın bitişik olup olmaması birşey değiştirmiyor sanırım.
+from .models import BlogPost
+
+def index(request):
+    """Blog projesi için home page"""
+    blog_post = BlogPost.objects.order_by('date_added')
+    context = {'index': blog_post}
+    return render(request, 'blogs/index.html', context)
